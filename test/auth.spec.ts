90ff90c4540e1f8bdf6ef82bcf8c4757433ad4c2
aleksey.dybov <aleksey.dybov@iqoption.com>
2024-05-23 17:15:29 +0300

    added after test handler

--- test/auth.spec.ts
+++ test/auth.spec.ts
@@ -4,20 +4,18 @@ import {User} from "./data/types";
 import {getUserByTitle} from "./setup/userUtils";
 
 describe('Authentication with login and password', () => {
+    let sdk: QuadcodeClientSdk;
     const user = getUserByTitle('regular_user') as User;
     const wsURL = process.env.WS_URL as string;
     const apiUrl = process.env.API_URL as string;
 
+    after(async function () {
+        await sdk.shutdown();
+    });
+
     it('should authenticate user with valid credentials', async () => {
-        const sdk = await QuadcodeClientSdk.create(
-            wsURL,
-            82,
-            new LoginPasswordAuthMethod(apiUrl, user.email, user.password));
-        try {
-            expect((await sdk.balances()).getBalances().length, "Invalid balances count").to.be.above(0);
-        } finally {
-            await sdk.shutdown();
-        }
+        sdk = await QuadcodeClientSdk.create(wsURL, 82, new LoginPasswordAuthMethod(apiUrl, user.email, user.password));
+        expect((await sdk.balances()).getBalances().length, "Invalid balances count").to.be.above(0);
     });
 
     it('should not authenticate user with invalid credentials', async () => {
