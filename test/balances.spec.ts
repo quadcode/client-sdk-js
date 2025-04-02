5b1fb1f7115b55ed9719fea1706aa8aa9d4b269f
aleksey.dybov <aleksey.dybov@quadcode.com>
2025-04-02 11:50:53 +0300

    fix test

--- test/balances.spec.ts
+++ test/balances.spec.ts
@@ -1,4 +1,4 @@
-import {Balance, Balances, BalanceType, ClientSdk, LoginPasswordAuthMethod, TurboOptionsDirection} from "../src";
+import {Balance, Balances, BalanceType, BinaryOptionsDirection, ClientSdk, LoginPasswordAuthMethod} from "../src";
 import {API_URL, User, WS_URL} from "./vars";
 import {getUserByTitle} from "./utils/userUtils";
 import {waitForCondition} from "./utils/waiters";
@@ -25,25 +25,25 @@ describe('Balances', () => {
     }
 
     async function openOption(balance: Balance, amount: number) {
-        const turboOptions = await sdk.turboOptions();
-        const turboOptionsActiveInstruments = await turboOptions.getActives()
+        const options = await sdk.binaryOptions();
+        const activeInstruments = await options.getActives()
             .filter(active => active.canBeBoughtAt(sdk.currentTime()))[0].instruments();
-        const availableForBuyAt = turboOptionsActiveInstruments.getAvailableForBuyAt(sdk.currentTime())[0];
-        await turboOptions.buy(availableForBuyAt, TurboOptionsDirection.Put, amount, balance)
+        const availableForBuyAt = activeInstruments.getAvailableForBuyAt(sdk.currentTime())[0];
+        await options.buy(availableForBuyAt, BinaryOptionsDirection.Put, amount, balance)
     }
 
     it('should reset demo balance', async () => {
         const balance = getBalance(BalanceType.Demo);
         const balanceAmount = balance.amount;
         const defaultBalanceAmount = 10000;
+        const amount = balanceAmount - defaultBalanceAmount + 1;
         if (balanceAmount >= defaultBalanceAmount) {
-            const amount = balanceAmount - defaultBalanceAmount + 1;
             await openOption(balance, amount);
-            expect(await waitForCondition(() => balance.amount === balanceAmount - amount, 2000)).to.be.true;
+            await waitForCondition(() => balance.amount !== defaultBalanceAmount, 3000);
             expect(balance.amount, "Balance amount should be changed").eq(balanceAmount - amount);
         }
         await balance.resetDemoBalance();
-        expect(await waitForCondition(() => balance.amount === defaultBalanceAmount, 2000)).to.be.true;
+        await waitForCondition(() => balance.amount !== balanceAmount - amount, 3000);
         expect(balance.amount, "Resent is not working, balance wasn't changed").eq(defaultBalanceAmount);
     });
 
