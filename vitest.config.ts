082550c4bb6c9ecca920b3a3c186810c168092bc
aleksey.dybov <aleksey.dybov@quadcode.com>
2025-12-01 12:40:15 +0300

    set hookTimeout to 65000, skip test

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,11 @@ import {defineConfig} from 'vitest/config'
 
 export default defineConfig({
     define: {
+        hookTimeout: 600000,
         IS_BROWSER: false
     },
     test: {
+        hookTimeout: 65000,
         testTimeout: 35000,
         include: ['./test/*.spec.{ts,js}'],
     },
