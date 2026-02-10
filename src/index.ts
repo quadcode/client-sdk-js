d5f8a6b013ab45b2e64550b8263ffa6b4120b86d
zakir.chalimov <zakir.chalimov@quadcode.com>
2026-02-10 18:19:27 +0600

    Release 1.3.18

--- src/index.ts
+++ src/index.ts
@@ -7210,7 +7210,7 @@ class HttpApiClient {
             'Content-Type': 'application/json',
         }
         if (!this.isBrowser) {
-            headers['User-Agent'] = 'quadcode-client-sdk-js/1.3.17'
+            headers['User-Agent'] = 'quadcode-client-sdk-js/1.3.18'
         }
         const requestOptions = {
             method: request.method(),
@@ -7321,7 +7321,7 @@ class WsApiClient {
                     this.connection = new WebSocket(this.apiUrl, undefined, {
                         headers: {
                             'cookie': `platform=${this.platformId}`,
-                            'user-agent': 'quadcode-client-sdk-js/1.3.17'
+                            'user-agent': 'quadcode-client-sdk-js/1.3.18'
                         }
                     });
                 } else {
